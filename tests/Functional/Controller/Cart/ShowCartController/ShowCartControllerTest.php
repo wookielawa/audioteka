a9a128dcb782f3b44f4f341bf069e93b6c633f5a
wookie <lawanski@gmail.com>
2025-06-21 18:39:41 +0200

    let's follow TDD and align tests first

--- tests/Functional/Controller/Cart/ShowCartController/ShowCartControllerTest.php
+++ tests/Functional/Controller/Cart/ShowCartController/ShowCartControllerTest.php
@@ -24,19 +24,22 @@ class ShowCartControllerTest extends WebTestCase
             'total_price' => 10970,
             'products' => [
                 [
-                    'id' => '15e4a636-ef98-445b-86df-46e1cc0e10b5',
-                    'name' => 'Product 3',
-                    'price' => 4990,
+                    'id' => 'fbcb8c51-5dcc-4fd4-a4cd-ceb9b400bff7',
+                    'name' => 'Product 1',
+                    'price' => 1990,
+                    'quantity' => 1,
                 ],
                 [
                     'id' => '9670ea5b-d940-4593-a2ac-4589be784203',
                     'name' => 'Product 2',
                     'price' => 3990,
+                    'quantity' => 1,
                 ],
                 [
-                    'id' => 'fbcb8c51-5dcc-4fd4-a4cd-ceb9b400bff7',
-                    'name' => 'Product 1',
-                    'price' => 1990,
+                    'id' => '15e4a636-ef98-445b-86df-46e1cc0e10b5',
+                    'name' => 'Product 3',
+                    'price' => 4990,
+                    'quantity' => 1,
                 ],
             ]
         ], $response);
