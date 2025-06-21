ac5d7b5c04844945a1037db6620430da2dff3cd3
wookie <lawanski@gmail.com>
2025-06-21 18:41:51 +0200

    align Cart entity to work with new relation

--- src/Service/Cart/Cart.php
+++ src/Service/Cart/Cart.php
@@ -2,15 +2,13 @@
 
 namespace App\Service\Cart;
 
-use App\Service\Catalog\Product;
-
 interface Cart
 {
     public function getId(): string;
     public function getTotalPrice(): int;
     public function isFull(): bool;
     /**
-     * @return Product[]
+     * @return CartProductInterface[]
      */
-    public function getProducts(): iterable;
+    public function getCartProducts(): iterable;
 }
