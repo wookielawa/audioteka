0d18ac1a913fec0aebadc65d98f0553e3516b1f0
wookie <lawanski@gmail.com>
2025-06-22 08:46:51 +0200

    let's move methods from the repository to the service, tests are now passing

--- src/Repository/CartRepository.php
+++ src/Repository/CartRepository.php
@@ -2,62 +2,7 @@
 
 namespace App\Repository;
 
-use App\Entity\CartAddProductStatus;
-use App\Entity\Product;
-use App\Service\Cart\Cart;
-use Doctrine\ORM\EntityManagerInterface;
-use Ramsey\Uuid\Uuid;
+use Doctrine\ORM\EntityRepository;
 
-class CartRepository
-{
-    public function __construct(private EntityManagerInterface $entityManager) {}
-
-    public function addProduct(string $operationId, string $cartId, string $productId): void
-    {
-        $cartAddProductStatus = new CartAddProductStatus($operationId);
-
-        $cart = $this->entityManager->find(\App\Entity\Cart::class, $cartId);
-        $product = $this->entityManager->find(Product::class, $productId);
-
-        if ($cart && $cart->isFull()) {
-            $cartAddProductStatus->setMessage('Cart is full.');
-
-            $this->entityManager->persist($cartAddProductStatus);
-            $this->entityManager->flush();
-
-            return;
-        }
-
-        if ($cart && $product) {
-            $cart->addProduct($product);
-            $this->entityManager->persist($cart);
-            $this->entityManager->flush();
-
-            $cartAddProductStatus->setMessage('OK');
-            $this->entityManager->persist($cartAddProductStatus);
-            $this->entityManager->flush();
-        }
-    }
-
-    public function removeProduct(string $cartId, string $productId): void
-    {
-        $cart = $this->entityManager->find(\App\Entity\Cart::class, $cartId);
-        $product = $this->entityManager->find(Product::class, $productId);
-
-        if ($cart && $product && $cart->hasProduct($product)) {
-            $cart->removeProduct($product);
-            $this->entityManager->persist($cart);
-            $this->entityManager->flush();
-        }
-    }
-
-    public function create(): Cart
-    {
-        $cart = new \App\Entity\Cart(Uuid::uuid4()->toString());
-
-        $this->entityManager->persist($cart);
-        $this->entityManager->flush();
-
-        return $cart;
-    }
-}
+class CartRepository extends EntityRepository
+{}
--- src/Service/Cart/CartService.php
+++ src/Service/Cart/CartService.php
@@ -2,20 +2,61 @@
 
 namespace App\Service\Cart;
 
+use App\Entity\CartAddProductStatus;
+use App\Entity\Product;
+use Doctrine\ORM\EntityManagerInterface;
+use Ramsey\Uuid\Uuid;
+
 class CartService implements CartServiceInterface
 {
+    public function __construct(private EntityManagerInterface $entityManager) {}
+
     public function addProduct(string $operationId, string $cartId, string $productId): void
     {
-        // TODO: Implement addProduct() method.
+        $cartAddProductStatus = new CartAddProductStatus($operationId);
+
+        $cart = $this->entityManager->find(\App\Entity\Cart::class, $cartId);
+        $product = $this->entityManager->find(Product::class, $productId);
+
+        if ($cart && $cart->isFull()) {
+            $cartAddProductStatus->setMessage('Cart is full.');
+
+            $this->entityManager->persist($cartAddProductStatus);
+            $this->entityManager->flush();
+
+            return;
+        }
+
+        if ($cart && $product) {
+            $cart->addProduct($product);
+            $this->entityManager->persist($cart);
+            $this->entityManager->flush();
+
+            $cartAddProductStatus->setMessage('OK');
+            $this->entityManager->persist($cartAddProductStatus);
+            $this->entityManager->flush();
+        }
     }
 
     public function removeProduct(string $cartId, string $productId): void
     {
-        // TODO: Implement removeProduct() method.
+        $cart = $this->entityManager->find(\App\Entity\Cart::class, $cartId);
+        $product = $this->entityManager->find(Product::class, $productId);
+
+        if ($cart && $product && $cart->hasProduct($product)) {
+            $cart->removeProduct($product);
+            $this->entityManager->persist($cart);
+            $this->entityManager->flush();
+        }
     }
 
     public function create(): Cart
     {
-        // TODO: Implement create() method.
+        $cart = new \App\Entity\Cart(Uuid::uuid4()->toString());
+
+        $this->entityManager->persist($cart);
+        $this->entityManager->flush();
+
+        return $cart;
     }
 }
