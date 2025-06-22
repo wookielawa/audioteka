12568d5250ad4716499b89eaaecc3249f2e09919
wookie <lawanski@gmail.com>
2025-06-22 08:43:18 +0200

    create a new class that will take over the responsibilities of the repository and handle the logic

---
+++ src/Service/Cart/CartService.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace App\Service\Cart;
+
+class CartService implements CartServiceInterface
+{
+    public function addProduct(string $operationId, string $cartId, string $productId): void
+    {
+        // TODO: Implement addProduct() method.
+    }
+
+    public function removeProduct(string $cartId, string $productId): void
+    {
+        // TODO: Implement removeProduct() method.
+    }
+
+    public function create(): Cart
+    {
+        // TODO: Implement create() method.
+    }
+}
